24df6cd7d5e3f7148e0f76986004b8ed992be591
leonardo <leoldss19@gmail.com>
2024-11-12 18:29:51 -0300

    instalação do react-router-dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,18 +7,23 @@ const Header = () => {
         <header>
 
             <div className='d-flex flex-row gap-4 m-4'>
+
                 <Logo />
+
                 <div className="input-group input-group-lg">
                     <input type="search" className="form-control" placeholder="Pesquisar Produto..."/>
                     <span className="input-group-text"><i class="bi bi-search"></i></span>
                 </div>
+
                 <div className='d-flex my-2 gap-2'>
                     <button href="#" className='btn btn-link text-nowrap'>Cadastre-se</button>
                     <button type='button' className='btn btn-primary px-4'>Entrar</button>
                 </div>
+
                 <div className='my-auto mx-4'>
                     <img src={MiniCart} alt="icone carrinho de compras" />
                 </div>
+                
             </div>
 
         </header>
